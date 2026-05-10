ad19e7c7f92b997e0e6397022b4e708b48ef2967
Lovely Dehar <190919076+Lovelydehar3@users.noreply.github.com>
2026-05-10 16:44:25 +0530

    final bugs fixed

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -21,7 +21,7 @@ const apiClient = axios.create({
   baseURL: API_BASE_URL,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
-  timeout: 35000
+  timeout: 60000
 });
 
 let activeBaseIndex = 0;
